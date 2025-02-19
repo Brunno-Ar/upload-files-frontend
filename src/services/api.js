d118d1b69a2f1fe6a7062e2407536261e456a138
Bruno Araujo <Brunnoaraujoc@gmail.com>
2025-02-19 16:10:17 -0300

    fix: Integrando o back-end com o render

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000", // URL do seu backend
+  baseURL: "https://api-upload-arquivos.onrender.com", // URL do seu backend
 });
 
 export default api;
